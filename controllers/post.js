b7727eea7aeb5d6bb29069f6f1a74a24e7c59e56
Michael Carroll <michaelcarroll1999@gmail.com>
2020-11-22 16:40:44 -0500

    sort test

--- controllers/post.js
+++ controllers/post.js
@@ -165,8 +165,7 @@ exports.getUserPage = async (req, res, next) => {
     
     try {
         user = await User.findById(getPageByUserId).populate('posts');
-        console.log(user.posts.sort({"createdAt": "desc"}))
-        const posts = user.posts.sort({"createdAt": "desc"}).populate({path: 'comments', populate: { path:  'creator', model: 'User' }});
+        const posts = user.posts.populate({path: 'comments', populate: { path:  'creator', model: 'User' }});
         if(!user){
             const error = Error("No user found");
             error.statusCode = 404;
